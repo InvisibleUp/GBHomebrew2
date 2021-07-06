aa74654549aae23cbe1735319e401026412425f7
InvisibleUp <invisibleup@outlook.com>
2021-07-05 18:02:43 -0700

    Add music support

--- build.bat
+++ build.bat
@@ -10,9 +10,18 @@ for %%f in (gfx/*.png) do rgbds\rgbgfx -h -o gfx/bin/%%f.bin gfx/%%f
 if %errorlevel% neq 0 call :exit 1
 
 REM Run assembler, linker, etc.
+:: Assemble main game into an object
 rgbds\rgbasm -o.\bin\game.obj .\bin\src.z80
 if %errorlevel% neq 0 call :exit 1
-rgbds\rgblink -m.\bin\game.map -n.\bin\game.sym -o.\bin\game.gb .\bin\game.obj
+:: Assemble the hUGEDriver source into an object
+rgbds\rgbasm -o.\bin\hUGEDriver.obj .\include\hUGEDriver.asm
+if %errorlevel% neq 0 call :exit 1
+:: Assemble songs into an object
+rgbds\rgbasm -o.\bin\sample_song.obj -i.. .\include\sample_song.asm
+if %errorlevel% neq 0 call :exit 1
+
+:: Link everything together
+rgbds\rgblink -m.\bin\game.map -n.\bin\game.sym -o.\bin\game.gb .\bin\game.obj .\bin\hUGEDriver.obj .\bin\sample_song.obj
 if %errorlevel% neq 0 call :exit 1
 rgbds\rgbfix -p0 -v .\bin\game.gb
 if %errorlevel% neq 0 call :exit 1
